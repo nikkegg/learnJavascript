d7dfc0eed363fb2cc468691840af62e0a2e8aec6
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-12-03 15:06:12 +0000

    added transaction listings

--- Bankist/script.js
+++ Bankist/script.js
@@ -64,22 +64,24 @@ const inputLoanAmount = document.querySelector(
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function(movements) {
+  containerMovements.innerHTML = '';
+  movements.forEach(function(mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal'
+    const html =
+      `
+            <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__date"></div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  })
+}
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-
-
+displayMovements(account1.movements);
 
 
 
