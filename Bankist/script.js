0bc01ede297d8cfc6369d824ea39565fd045c0f9
Nikita Visencuk Visenko <cereshnia@gmail.com>
2020-12-10 11:38:04 +0000

    added comments, shorter querySelecto function

--- Bankist/script.js
+++ Bankist/script.js
@@ -4,7 +4,7 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
+// Fake Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
@@ -35,42 +35,54 @@ const account4 = {
 
 const accounts = [account1, account2, account3, account4];
 
+// Shorter querySelector
+const select = element => document.querySelector(element)
+
 // Elements
-const labelWelcome = document.querySelector('.welcome');
-const labelDate = document.querySelector('.date');
-const labelBalance = document.querySelector('.balance__value');
-const labelSumIn = document.querySelector('.summary__value--in');
-const labelSumOut = document.querySelector('.summary__value--out');
-const labelSumInterest = document.querySelector(
+const labelWelcome = select('.welcome');
+const labelDate = select('.date');
+const labelBalance = select('.balance__value');
+const labelSumIn = select('.summary__value--in');
+const labelSumOut = select('.summary__value--out');
+const labelSumInterest = select(
   '.summary__value--interest');
-const labelTimer = document.querySelector('.timer');
+const labelTimer = select('.timer');
 
-const containerApp = document.querySelector('.app');
-const containerMovements = document.querySelector('.movements');
+const containerApp = select('.app');
+const containerMovements = select('.movements');
 
-const btnLogin = document.querySelector('.login__btn');
-const btnTransfer = document.querySelector('.form__btn--transfer');
-const btnLoan = document.querySelector('.form__btn--loan');
-const btnClose = document.querySelector('.form__btn--close');
-const btnSort = document.querySelector('.btn--sort');
+const btnLogin = select('.login__btn');
+const btnTransfer = select('.form__btn--transfer');
+const btnLoan = select('.form__btn--loan');
+const btnClose = select('.form__btn--close');
+const btnSort = select('.btn--sort');
 
-const inputLoginUsername = document.querySelector('.login__input--user');
-const inputLoginPin = document.querySelector('.login__input--pin');
-const inputTransferTo = document.querySelector('.form__input--to');
-const inputTransferAmount = document.querySelector(
+const inputLoginUsername = select('.login__input--user');
+const inputLoginPin = select('.login__input--pin');
+const inputTransferTo = select('.form__input--to');
+const inputTransferAmount = select(
   '.form__input--amount');
-const inputLoanAmount = document.querySelector(
+const inputLoanAmount = select(
   '.form__input--loan-amount');
-const inputCloseUsername = document.querySelector('.form__input--user');
-const inputClosePin = document.querySelector('.form__input--pin');
+const inputCloseUsername = select('.form__input--user');
+const inputClosePin = select('.form__input--pin');
 
+// Creating usernames based on initials of an owner
+accounts.forEach(function(account) {
+  const names = account.owner.split(' ');
+  let initials = '';
+  names.forEach(name => initials += name.toLowerCase()[0]);
+  account['username'] = initials;
+})
+
+// Displaying transactions
 const displayMovements = function(movements) {
   containerMovements.innerHTML = '';
   movements.forEach(function(mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal'
     const html =
       `
-            <div class="movements__row">
+        <div class="movements__row">
           <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
           <div class="movements__date"></div>
           <div class="movements__value">${mov}</div>
