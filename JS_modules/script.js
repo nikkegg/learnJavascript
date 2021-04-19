b63c9497d46ae1b7e95ed5dfa1a8ff411d305fcc
Nikita Visencuk Visenko <cereshnia@gmail.com>
2021-04-19 13:17:36 +0100

    module system

--- JS_modules/script.js
+++ JS_modules/script.js
@@ -3,5 +3,12 @@ import { addToCart } from './shoppingCart.js';
 // importing everything. Still have to export things from the other file, otherwise
 // error will be thrown.
 import * as ATC from './shoppingCart.js';
-addToCart('bread', 5)
+// default export are used when only one thing from a module needs to be imported. Default imports do no use curly braces
+addToCart('bread', 5);
+addToCart('mustard', 5);
 console.log(ATC.cart)
+
+// importing and exporting in node.js
+
+// to export: export.itemName is exported
+// to import: const { itemName} = require('./rel_path')
